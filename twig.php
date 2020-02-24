3828c044417bf47923d70cdb37a9846456e36b52
eaglecrazy <eaglecrazy@mail.ru>
2020-02-24 22:34:47 +0300

    Делаю ДЗ №3

--- twig.php
+++ twig.php
@@ -1,19 +1,28 @@
 <?php
-
-require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+// подгружаем и активируем авто-загрузчик Twig-а
+require_once 'twig/Autoloader.php';
 Twig_Autoloader::register();
 
 try {
+    // указывае где хранятся шаблоны
     $loader = new Twig_Loader_Filesystem('templates');
-    $twig = new Twig_Environment();
+
+    // инициализируем Twig
+    $twig = new Twig_Environment($loader);
+
+    // подгружаем шаблон
     $template = $twig->loadTemplate('thanks.tmpl');
+
+    // передаём в шаблон переменные и значения
+    // выводим сформированное содержание
+
+    $content = $template->render(array(
+        'name' => 'Clark Kent',
+        'username' => 'ckent',
+        'password' => 'krypt0n1te',
+    ));
+    echo $content;
+
 } catch (Exception $e) {
-
+    die ('ERROR: ' . $e->getMessage());
 }
-
-
-
-
-//    $content = $template->render(array(
-//        'username' => 'username'));
-//    echo $content;
